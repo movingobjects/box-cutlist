02b7e9b4a971e76ee7d433b5827a0e18fbcbc4ff
Scott Thiessen <scott@movingobjects.io>
2026-09-23 13:14:21 -0700

    Add GH pages deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
-export default defineConfig({ plugins: [react()] });
+export default defineConfig({
+  base: './',
+  plugins: [react()]
+});
